f3f3db52447d22ef2a1767d6137597e328ccc934
Dragodreg <Dragodreg@gmail.com>
2016-11-09 15:36:41 +0000

    Update init.pp

--- init.pp
+++ init.pp
@@ -1,2 +1,36 @@
-jeee
+class package1{
+Package {ensure => "installed", allowcdrom => "true"}
+package {"apache2":}
+package {"vlc":}
+package {"gedit":}
+package {"ssh":}
+
+file {"/var/www/html/index.html":
+content => "Welcome.\n",
+}
+
+file {"/etc/apache2/mods-enabled/userdir.conf":
+ensure => "link",
+target => "/etc/apache2/mods-available/userdir.conf",
+notify => Service["apache2"],
+}
+
+file {"/etc/apache2/mods-enabled/userdir.load":
+ensure => "link",
+target => "/etc/apache2/mods-available/userdir.load",
+notify => Service["apache2"],
+}
+
+service {"apache2":
+ensure => "true",
+enable => "true",
+# provider => "systemd",
+}
+}
+
+
+
+
+
+
 
